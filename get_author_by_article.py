3ad2dff96beaa413cc5f64d708a8e096a3b1c9ea
Roman Dryndik <dryndikroman@gmail.com>
2023-05-20 12:41:18 +0200

    Add another relationship example.

---
+++ get_author_by_article.py
@@ -0,0 +1,11 @@
+from models import Article
+from session import session
+
+
+def main():
+    article = session.query(Article).get(1)
+    print(article, article.author)
+
+
+if __name__ == "__main__":
+    main()
